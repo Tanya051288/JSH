9864e01a9e427f740cba0be1f2d33be2175bd1d6
Tanya051288 <142173622+Tanya051288@users.noreply.github.com>
2023-09-14 14:28:05 +0300

    Add files via upload
    add js

---
+++ app.js
@@ -0,0 +1,76 @@
+let firstName="Bob"
+let lastName="Black"
+firstName + " " + lastName
+console.log (firstName+ " " + lastName)
+
+
+let result=Math.pow (10, 6)
+console.log(result) 
+console.log (Math.floor (Math.random()*50))
+
+let number= 10
+if (number<=10) {
+    console.log ("negative value")
+}
+ else if (number>=10) {
+    console.log ("positive value")
+}
+
+let text="Hello".length>1
+ console.log ("Hello" [3] )
+
+ 
+    let month=9;
+ 
+ if (month=== 1){
+    console.log ("January");
+     }
+     else if (month===2){
+    console.log ("February")  
+     }else if (month===3){
+        console.log ("March") 
+     }else if (month===3){
+        console.log ("March")
+     }else if (month===4){
+        console.log ("Apriel")
+     }else if (month===5){
+        console.log ("May")
+     }else if(month===6){
+        console.log ("June")
+     }else if(month===7){
+        console.log ("July") 
+     }else if(month===8){
+        console.log ("August")
+     }else if(month===9){
+        console.log ("September") 
+     }else if(month===10){
+        console.log ("Oktober")
+     }else if(month===11){
+        console.log ("November")
+     }else if(month===12){
+         console.log ("December")
+     }  
+     else{
+      console.log ("month not found!")}
+
+      let password="ghfghfjkhgjl"
+      if (password.length>5) {
+         if (password.indexOf ('&')===-1){
+             console.log ('this password is strong');
+         } else{
+             console.log ('this password is weak')
+         }   
+         }
+   let a=20
+   let b=40
+   a+b
+   console.log (a+b)
+   
+
+   let textOne="dgdhgkfkhjhgjhgjhjjhjh"
+   let textTwo="xxcbchgvjhgjhgh"
+   if  (textOne.length=textTwo.length) {
+      console.log ("those items are strings and they’re equal");
+   } else{
+      console.log ("items are not compareble")
+   } 
